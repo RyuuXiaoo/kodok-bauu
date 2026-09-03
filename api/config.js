77bd01055b295b272954f360a9065f9ea6a891be
RyuuXiao <164801435+RyuuXiaoo@users.noreply.github.com>
2026-09-03 19:39:45 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 // Konfigurasi fallback untuk deployment yang tidak memasang Environment Variables.
 // Isi FONNTE_API_TOKEN dengan token API Fonnte milik Anda sebelum deployment.
 module.exports = {
-  FONNTE_API_TOKEN: '',
+  FONNTE_API_TOKEN: 'EFLkpC9QVhgBSHNcNwZX',
   FONNTE_OWNER_NUMBERS: '6285143469870,6283849126096'
 };
